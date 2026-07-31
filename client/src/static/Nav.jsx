4e25c8e1a4be600f40803dd5e3666ad2b422e0b1
Lucia Alday <luciaalday03@gmail.com>
2026-07-30 17:15:20 -0700

    Mobile menu compatibility

--- client/src/static/Nav.jsx
+++ client/src/static/Nav.jsx
@@ -1,14 +1,20 @@
+import { useState } from 'react';
 import { Link } from "react-router-dom";
 
 export default function Nav() {
+  const [open, setOpen] = useState(true);
+
   return (
-    <nav>
-      <Link className="nav-link" to='/'>Home</Link>
-      <Link className="nav-link" to='/info'>Information</Link>
-      <Link className="nav-link" to='/view'>Databases</Link>
-      <Link className="nav-link" to='/visit'>Visitation Guidelines</Link>
-      <Link className="nav-link" to='/design'>Design Guidelines</Link>
-      <Link className="nav-link" to='/contact'>Contact</Link>
-    </nav>
+    <>
+      <button className="nav-button" onClick={()=>setOpen(!open)} ><b>&#9776;</b></button>
+      <nav className={open ? '' : 'closed'}>
+        <Link className="nav-link" to='/'>Home</Link>
+        <Link className="nav-link" to='/info'>Information</Link>
+        <Link className="nav-link" to='/view'>Databases</Link>
+        <Link className="nav-link" to='/visit'>Visitation Guidelines</Link>
+        <Link className="nav-link" to='/design'>Design Guidelines</Link>
+        <Link className="nav-link" to='/contact'>Contact</Link>
+      </nav>
+    </>
   )
 }
